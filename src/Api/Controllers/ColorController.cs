cef5cbff704f17a212d1ace777614eb44a13c301
Ali <105427281+Alilock@users.noreply.github.com>
2023-02-01 19:56:22 +0400

    color

---
+++ src/Api/Controllers/ColorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Application.CQRS.ColorAllQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorController :ControllerBase
+	{
+
+        private readonly IMediator mediator;
+
+        public ColorController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromRoute] ColorAllQuery query)
+        {
+            var response = await mediator.Send(query);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
+
---
+++ src/Application/CQRS/ColorAllQuery/ColorAllQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Application.CQRS.ColorModule;
+using Application.DbContext;
+using Domain.Entities;
+using Domain.Responses;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CQRS.ColorAllQuery
+{
+	public class ColorAllQuery :IRequest<Response<ICollection<Color>>>
+	{
+        public class ColorAllQueryHandler : IRequestHandler<ColorAllQuery, Response<ICollection<Color>>>
+        {
+            private readonly AppDbContext db;
+
+            public ColorAllQueryHandler(AppDbContext db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Response<ICollection<Color>>> Handle(ColorAllQuery request, CancellationToken cancellationToken)
+            {
+                var data = await db.Colors.Where(g => g.DeletedDate == null).ToListAsync(cancellationToken);
+                var response = new Response<ICollection<Color>>();
+
+                if (data == null)
+                {
+                    response.StatusCode = 404;
+                    response.Message = "NotFound";
+                    return response;
+                }
+
+                response.StatusCode = 200;
+                response.Message = "Ok";
+                response.Data = data;
+                return response;
+
+            }
+        }
+    }
+}
+

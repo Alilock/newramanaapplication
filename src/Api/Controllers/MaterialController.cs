d7330cd8235b1d9f5427d3617273c21bec2b0eba
Ali <105427281+Alilock@users.noreply.github.com>
2023-02-01 18:37:12 +0400

    a

---
+++ src/Api/Controllers/MaterialController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Application.CQRS.GenderModule;
+using Application.CQRS.MaterialModule;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialController :ControllerBase
+	{
+        private readonly IMediator mediator;
+
+        public MaterialController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromRoute] MaterialAllQuery query)
+        {
+            var response = await mediator.Send(query);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
+
---
+++ src/Application/CQRS/MaterialModule/MaterialAllQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Application.CQRS.GenderModule;
+using Application.DbContext;
+using Domain.Entities;
+using Domain.Responses;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CQRS.MaterialModule
+{
+	public class MaterialAllQuery : IRequest<Response<ICollection<Material>>>
+	{
+        public class MaterialAllQueryHandler : IRequestHandler<MaterialAllQuery, Response<ICollection<Material>>>
+        {
+            private readonly AppDbContext db;
+
+            public MaterialAllQueryHandler(AppDbContext db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Response<ICollection<Material>>> Handle(MaterialAllQuery request, CancellationToken cancellationToken)
+            {
+                var data = await db.Materials.Where(g => g.DeletedDate == null).ToListAsync(cancellationToken);
+                var response = new Response<ICollection<Material>>();
+
+                if (data == null)
+                {
+                    response.StatusCode = 404;
+                    response.Message = "NotFound";
+                    return response;
+                }
+
+                response.StatusCode = 200;
+                response.Message = "Ok";
+                response.Data = data;
+                return response;
+
+            }
+        }
+    }
+}
+
